efc2594a9831931517bbc2ae530a05f533c1eaa3
Stephen A. Balaban <stephen@stephenbalaban.com>
2012-06-30 15:39:02 -0700

    Added project ++ readme

---
+++ src/Feed_Forward_Neuralnet/core.clj
@@ -0,0 +1,1 @@
+(ns Feed-Forward-Neuralnet.core)
---
+++ test/Feed_Forward_Neuralnet/test/core.clj
@@ -0,0 +1,6 @@
+(ns Feed-Forward-Neuralnet.test.core
+  (:use [Feed-Forward-Neuralnet.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
